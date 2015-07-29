8f9dc0766359427f681dca923000d01201cda04e
savidlin <josephmoran@live.com>
2015-07-29 14:23:18 -0400

    added image gallery for adopt pages

--- js/modal.js
+++ js/modal.js
@@ -7,7 +7,7 @@ else if (document.location.pathname.indexOf("dogs") > -1) {
     loadDogImages('/js/dogs.json');
 } 
 else {
-    console.error('unable to load images')
+    console.error('unable to load images');
 }
 
 });
@@ -18,26 +18,34 @@ function loadDogImages(url) {
     var attributes = new Array();
     var imageGallery = $('.image-gallery');
     var imageGalleryThumbnail = $('.image-gallery .thumbnail');
+
     for (var i=0; i<json.dogs.length; i++) {
         var dog = json.dogs[i];
-        var imgUrl = 'http://placehold.it/200x200';
+        var imgUrl = dog.image;
         var thumbnail = imageGalleryThumbnail.clone();
         thumbnail.removeClass('hidden-lg');
         thumbnail.removeClass('hidden-md');
         thumbnail.removeClass('hidden-sm');
         thumbnail.removeClass('hidden-xs');
+        thumbnail.data('thumb',i);
         thumbnail.find('ul li').text(dog.name);
         thumbnail.find('img').attr('src', imgUrl);
         imageGallery.append(thumbnail);
-        attributes.push({img: imgUrl, head:dog.name , name: dog.name, age: dog.age}); 
+        attributes.push({img: imgUrl, name: dog.name, age: dog.age, gender: dog.gender, breed: dog.breed, color: dog.color, behavior: dog.behavior, needs: dog.needs}); 
     }   
     
     function setModalContent($thumbNumber) {
-        
-        $('#myModal .tmb-gray h3').text(attributes[$thumbNumber].head);
-        $('#myModal .tmb-gray h3 + p').text(attributes[$thumbNumber].name);
-        $('#myModal .tmb-gray .name').text(attributes[$thumbNumber].age);
+
         $('#myModal .modal-body img').last().attr('src',attributes[$thumbNumber].img);
+        $('#myModal .name').html('<b>Name:</b> ' + attributes[$thumbNumber].name);
+        $('#myModal .age').html('<b>Age:</b> ' + attributes[$thumbNumber].age);
+        $('#myModal .gender').html('<b>Gender:</b> ' + attributes[$thumbNumber].gender);
+        $('#myModal .breed').html('<b>Breed:</b> ' + attributes[$thumbNumber].breed);
+        $('#myModal .color').html('<b>Color:</b> ' + attributes[$thumbNumber].color);
+        $('#myModal .behavior').html('<b>Behaviors:</b> ' + attributes[$thumbNumber].behavior);
+        $('#myModal .needs').html('<b>Special Needs:</b> ' + attributes[$thumbNumber].needs);
+        $('#myModal .pet-name').html('Want to take ' + attributes[$thumbNumber].name + ' home?');
+
 
         /* set next and previous buttons */
         $('.prev-img').data('thumb',($thumbNumber - 1 >= 0) ? $thumbNumber - 1 : attributes.length - 1);
@@ -61,26 +69,32 @@ function loadCatImages(url) {
     var attributes = new Array();
     var imageGallery = $('.image-gallery');
     var imageGalleryThumbnail = $('.image-gallery .thumbnail');
+
     for (var i=0; i<json.cats.length; i++) {
         var cat = json.cats[i];
-        var imgUrl = 'http://placehold.it/200x200';
+        var imgUrl = cat.image;
         var thumbnail = imageGalleryThumbnail.clone();
         thumbnail.removeClass('hidden-lg');
         thumbnail.removeClass('hidden-md');
         thumbnail.removeClass('hidden-sm');
         thumbnail.removeClass('hidden-xs');
+        thumbnail.data('thumb',i);
         thumbnail.find('ul li').text(cat.name);
         thumbnail.find('img').attr('src', imgUrl);
         imageGallery.append(thumbnail);
-        attributes.push({img: imgUrl, head:cat.name , name: cat.name, age: cat.age}); 
+        attributes.push({img: imgUrl, name: cat.name, age: cat.age, gender: cat.gender, color: cat.color, needs: cat.needs}); 
     }   
     
     function setModalContent($thumbNumber) {
-        
-        $('#myModal .tmb-gray h3').text(attributes[$thumbNumber].head);
-        $('#myModal .tmb-gray h3 + p').text(attributes[$thumbNumber].name);
-        $('#myModal .tmb-gray .name').text(attributes[$thumbNumber].age);
+
         $('#myModal .modal-body img').last().attr('src',attributes[$thumbNumber].img);
+        $('#myModal .name').html('<b>Name:</b> ' + attributes[$thumbNumber].name);
+        $('#myModal .age').html('<b>Age:</b> ' + attributes[$thumbNumber].age);
+        $('#myModal .gender').html('<b>Gender:</b> ' + attributes[$thumbNumber].gender);
+        $('#myModal .color').html('<b>Color:</b> ' + attributes[$thumbNumber].color);
+        $('#myModal .needs').html('<b>Special Needs:</b> ' + attributes[$thumbNumber].needs);
+        $('#myModal .pet-name').html('Want to take ' + attributes[$thumbNumber].name + ' home?');
+
 
         /* set next and previous buttons */
         $('.prev-img').data('thumb',($thumbNumber - 1 >= 0) ? $thumbNumber - 1 : attributes.length - 1);
